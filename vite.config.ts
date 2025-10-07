4c7abc1a06989b94a29318401a8dcb7774513ae8
dostoyanov <dstoyanov@vedamo.com>
2025-10-07 13:27:29 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/fm-7-recipe-finder",
+  // base: process.env.VITE_BASE_PATH || "/fm-7-recipe-finder",
+  base: process.env.VERCEL ? "/" : "/fm-7-recipe-finder/",
 });
